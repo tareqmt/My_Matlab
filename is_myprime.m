1a9d0c6858ad1a92dcbce3490e5928b631fafc4a
Tareq AlTalmas <52353714+tareqmt@users.noreply.github.com>
2020-11-30 07:19:09 +0800

    Update is_myprime.m

--- is_myprime.m
+++ is_myprime.m
@@ -1,14 +1,14 @@
-
+%%%Find if n is a prime number or not
 function out = is_myprime(n)
 
-if n <= 0
+if n <= 0                      %%% negative numbers are not primes 
     out = [num2str(n) ' is not a prime number'];
-elseif n == 1
+elseif n == 1                   %%% 1 is not a prime 
     out = [num2str(n) ' is not a prime number'];
-elseif n == 2
+elseif n == 2                       %%% 2 is a prime 
     out = [num2str(n) ' is a prime number'];
 else
-    
+    %%%%check numbers greater than 2 
     v = [3:n];
     XX = mod(n,v);
     B = (XX == 0 );
@@ -22,3 +22,4 @@ end
 end
 
 
+%%%%Please leave you comment about the code :)
